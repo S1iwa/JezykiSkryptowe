abca23bf989a97dfc2adbded249256aedbbf23af
Gasin <tgasinski2014@gmail.com>
2026-06-16 15:49:31 +0200

    Adding function for showing Logging page in app.js

--- schedule_app/static/schedule_app/js/app.js
+++ schedule_app/static/schedule_app/js/app.js
@@ -1,8 +1,36 @@
+// Funkcja pokazLogowanie - Strona logowania
+function pokazLogowanie(app) {
+    app.innerHTML = `
+        <div class="formularz-logowania">
+            <h2>Logowanie</h2>
+            <p>Jeden formularz dla studenta, wykładowcy i planisty.</p>
+            <div id="blad-logowania" class="blad hidden"></div>
+            <div class="pole">
+                <label for="pole-email">E-mail</label>
+                <input type="email" id="pole-email" placeholder="np. jan@student.pl">
+            </div>
+            <div class="pole">
+                <label for="pole-haslo">Hasło</label>
+                <input type="password" id="pole-haslo" placeholder="hasło">
+            </div>
+            <button id="przycisk-login">Zaloguj się</button>
+        </div>
+    `;
+    document.getElementById('przycisk-login').onclick = function() {
+        alert('Logowanie.');
+    };
+}
+
+
+
+
+
+// Funkcja router - Przeglądarka sprawdza aktualny adres URL i wyświetla odpowiednią treść:
 function router() {
     var sciezka = window.location.pathname;
     var app = document.getElementById('app');
     if (sciezka === '/logowanie/') {
-        app.innerHTML = '<h1>Logowanie</h1>';
+        pokazLogowanie(app);
     } else if (sciezka === '/panel-studenta/') {
         app.innerHTML = '<h1>Panel studenta</h1>';
     } else if (sciezka === '/panel-wykladowcy/') {
